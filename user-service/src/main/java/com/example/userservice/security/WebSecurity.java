e041022b02762476927586046fe735e04beaee13
jjh5887 <wjdrnjsgh159@naver.com>
2022-02-23 22:56:58 +0900

    Configuration Service

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -24,6 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		// http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 		http.authorizeRequests().antMatchers("/**")
 			.hasIpAddress("172.30.1.37")
 			.and()
@@ -33,9 +34,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	}
 
 	private AuthenticationFilter getAuthenticationFilter() throws Exception {
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-		authenticationFilter.setAuthenticationManager(authenticationManager());
-
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(), userService, env);
+		// authenticationFilter.setAuthenticationManager(authenticationManager());
 		return authenticationFilter;
 	}
 
