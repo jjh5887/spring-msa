4043b1665217d09c93ad1f5b88dc8671d9f7769d
jjh5887 <wjdrnjsgh159@naver.com>
2022-03-06 14:23:33 +0900

    Catalog Microservice 컨테이너 가상화

--- catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumerConfig.java
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumerConfig.java
@@ -19,7 +19,7 @@ public class KafkaConsumerConfig {
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> properties = new HashMap<>();
-		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.0.101:9092");
 		// 토픽에 쌓여있는 메시지를 가져가는 Consumer 들을 Grouping 할 때 사용 (현재는 1개라 의미 X)
 		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
 		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
